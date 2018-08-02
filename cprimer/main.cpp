4221ebcd75c4f5a3d01f737be69ccba46ea3607a
larryhou <larryhou@larryhou-rmbp.local>
2018-08-02 16:55:03 +0800

    Initial Commit

---
+++ cprimer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cprimer
+//
+//  Created by larryhou on 2018/8/2.
+//  Copyright © 2018 larryhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
